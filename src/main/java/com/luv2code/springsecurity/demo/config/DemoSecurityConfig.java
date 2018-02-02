07f62d090606fd3440c5cee5a06649a240b58601
Alex20177 <alejandro.lopez207@gmail.com>
2018-02-01 18:51:28 -0600

    Second commit

--- src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -37,7 +37,9 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 			.loginPage("/showMyLoginPage")
 			.loginProcessingUrl("/authenticateTheUser")
 			.permitAll()
-			.and().logout().permitAll();	
+			.and().logout().permitAll()
+			.and()
+			.exceptionHandling().accessDeniedPage("/access-denied");
 	}
 		
 }
